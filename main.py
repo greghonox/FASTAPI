c8d5c49ee8bb61e8cefb751146227f065467e02f
gregorio honorato <gregoriohonorato@AZ028.local>
2022-10-13 22:32:54 -0300

    adding called in main

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
+
 
 @app.get('/')
 async def raiz() -> dict:
     return {'msg': 'MSG INIT'}
+
+@app.get('/mensagem')
+async def message() -> str:
+    return 'message fastapi'
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='0.0.0.0', 
+                port=8000, log_level='info', reload=True)
