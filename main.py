a2e39806d62ede9df1400b0a688c37a9bb310223
Gregorio Honorato <greghono@gmail.com>
2022-11-02 19:59:53 -0300

    adicionando docuemntacao\najustando o modelo para uso

--- main.py
+++ main.py
@@ -1,69 +1,66 @@
 from fastapi import FastAPI, HTTPException, status, Path, Query, Header, Depends
-from models import Cursos
-from typing import Any
+from models import Cursos, cursos
+from typing import Any, List
 import uvicorn
 
 from scripts.utils import choices
 
-app = FastAPI()
-
-aulas = {
-    1: {
-        'titulo': 'Aula de programação',
-        'aula': 111,
-        'horas': 56
-    },
-    2: {
-        'titulo': 'Aula de fastapi',
-        'aula': 11,
-        'horas': 51
-    },    
-}
+app = FastAPI(
+    title='Aprendendo fastApi',
+    version='0.0.1',
+    description='Aprendendo fastApi para ficar bom'
+    )
 
 
-@app.get('/')
+
+@app.get('/', description='APenas para saber que funciona',
+              summary='Raiz de tudo')
 async def raiz() -> dict:
     return {'msg': 'MSG INIT'}
 
-@app.get('/mensagem')
+@app.get('/mensagem', description='Para saber que funciona',
+                      summary='Um breve teste!')
 async def message() -> str:
     return 'message fastapi'
 
-@app.get('/cursos')
+@app.get('/cursos', description='Retornar todos os cursos que estão em memoria',
+                    summary=f'Lista de cursos {cursos}',
+                    response_model=List[Cursos])
 def get_cursos(request: Any = Depends(choices)):
-    return aulas
+    return cursos
 
-@app.get('/cursos/{curso_id}')
-def get_cursos(curso_id:int=Path(default=None, title='ID do curso', 
-              description='Deve ser entre 1, 10', gt=1, lt=11), request: Any = Depends(choices)) -> dict:
+@app.get('/cursos/{curso_id}', response_model=Cursos)
+def get_cursos_id(curso_id:int=Path(default=None, title='ID do curso', 
+              description='Deve ser entre 1, 10', 
+              gt=1, lt=11), request: Any = Depends(choices)) -> dict:
     try:
-        return aulas[curso_id]
+        return cursos[curso_id - 1]
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='curso não encontrado')
 
 @app.post('/cursos')
-def post_cursos(curso: Cursos, request: Any = Depends(choices)) -> dict:
-    if curso.id in aulas:
+def post_cursos(curso: Cursos) -> dict:
+    if curso.id in cursos:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                             detail=f'Curso já existe! {curso}')
-    next_id = len(aulas) + 1 if curso.id is None else curso.id
-    aulas[next_id] = curso
-    aulas[next_id].id = next_id
-    return aulas[next_id]
+    next_id = len(cursos) + 1 if curso.id is None else curso.id
+    cursos[next_id] = curso
+    cursos[next_id].id = next_id
+    return cursos[next_id]
         
 @app.put('/cursos/{curso_id}')
 def put_cursos(curso_id: int, curso: Cursos, request: Any = Depends(choices)) -> dict:
-    if curso_id in aulas:
-        aulas[curso_id] = curso
-        return aulas[curso_id]
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        return cursos[curso_id]
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail=f'Curso id {curso_id} nao foi encontrado')
 
 @app.delete('/cursos/{curso_id}')    
 def delete_cursos(curso_id: int, request: Any = Depends(choices)) -> dict:
-    if curso_id in aulas:
-        curso = aulas[curso_id]
-        del aulas[curso_id]
+    if curso_id in cursos:
+        curso = cursos[curso_id]
+        del cursos[curso_id]
         return curso
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail=f'Curso id {curso_id} nao foi encontrado')
--- models.py
+++ models.py
@@ -9,3 +9,10 @@ class Cursos(BaseModel):
     
     def __str__(self) -> str:
         return f'{self.id} {self.titulo} {self.aulas} {self.horas}'
+    
+
+
+cursos = [
+    Cursos(id=1, titulo='Aula de programação', aulas=11, horas=56),
+    Cursos(id=2, titulo='Aula de fastapi', aulas=11, horas=51),
+]
